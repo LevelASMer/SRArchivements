ebf89e32177859d2041431043b7d82600ec41f7b
Level ASMer <zx132435@naver.com>
2023-06-26 18:00:38 +0900

    1.1.0

---
+++ pages/components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className='text-center my-10 text-slate-600 dark:text-white'>
+            <h4 className='text-xl font-black'>스타레일 업적 사전 <sup>v0.4.0</sup></h4>
+        </footer>
+    )
+}
+
+export default Footer
